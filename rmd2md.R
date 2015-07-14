dfc1c530466b4d221e6b3a77cf36268dd4aa626b
edisz <eduardszoecs@gmail.com>
2015-07-14 02:21:46 +0200

    renamed source folder

--- rmd2md.R
+++ rmd2md.R
@@ -14,7 +14,7 @@
 #' @return nothing.
 #' @author Jason Bryer <jason@bryer.org> edited by Andy South
 rmd2md <- function(path_site = getwd(),
-                    dir_rmd = "_rmd",
+                    dir_rmd = "_source",
                     dir_md = "_posts",                              
                     #dir_images = "figures",
                     url_images = "figures/",
